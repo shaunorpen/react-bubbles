e9b6e61ff45b5ae1c59e0a21bd1c305029e05d97
Shaun Orpen <shaunorpen@Shauns-MacBook-Pro.local>
2019-10-18 08:28:03 +0100

    Add login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
 
-const Login = () => {
+const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [formData, setFormData] = useState({
+    username: 'Lambda School',
+    password: 'i<3Lambd4',
+  });
+
+  const handleChange = e => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  const handleSubmit = () => {
+    axios
+      .post('http://localhost:5000/api/login', formData)
+      .then(res => {
+        localStorage.setItem('token', res.data.payload);
+        setFormData({ username: '', password: '', });
+        props.history.push('/');
+      })
+      .catch(err => {
+        alert(err.message);
+      })
+  }
+
   return (
-    <>
+    <div>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+      <div>
+        <label htmlFor='username'>Username: </label>
+        <input name='username' type='text' value={formData.username} onChange={handleChange} />
+        <label htmlFor='password'>Password: </label>
+        <input name='password' type='password' value={formData.password} onChange={handleChange} />
+        <button onClick={handleSubmit} >Submit</button>
+      </div>
+    </div>
   );
 };
 
